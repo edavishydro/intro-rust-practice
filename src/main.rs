4bfd3aa6ed94cf0c884cd80a8544cc7c0fa007e5
Edward Davis <hello@edwdavis.com>
2021-01-28 14:47:50 -0600

    first commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+fn main() {
+    let mut arguments = std::env::args().skip(1);
+    let key = arguments.next().unwrap();
+    let value = arguments.next().unwrap();
+    println!("The key is '{}' and the value is '{}'", key, value);
+    let contents = format!("{}\t{}\n", key, value);
+    // std::fs::write("kv.db", contents);
+
+    let database = Database::new().expect("Database::new() crashed");
+}
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database, std::io::Error> {
+        // Read kv.db file
+        // let contents = match std::fs::read_to_string("kv.db") {
+        //     Ok(c) => c,
+        //     Err(error) => {
+        //         return Err(error);
+        //     }
+        // };
+        let mut map = HashMap::new();
+        let contents = std::fs::read_to_string("kv.db")?;
+        // Parse the string
+        // populate our map
+        for line in contents.lines() {
+            let mut chunks = line.splitn(2, '\t');
+            let key = chunks.next().expect("No key!");
+            let value = chunks.next().expect("No value!");
+            map.insert(key.to_owned(), value.to_owned());
+        }
+        Ok(Database { map: map })
+    }
+}
